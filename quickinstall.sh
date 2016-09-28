c059d545b75183aa6f2852fbea16a12c63d6765a
NikolayS <github2@samokhvalov.com>
2016-09-28 05:53:54 +0300

    Update quickinstall.sh

--- quickinstall.sh
+++ quickinstall.sh
@@ -1,3 +1,8 @@
+apt-get -v &> /dev/null && sudo apt update && sudo apt install -y tmux
+which yum &> /dev/null && yum update && yum install tmux
+which brew &> /dev/null && brew install tmux
+
 git clone https://github.com/NikolayS/tmux.git ~/tmuxtmp
 cp ~/tmuxtmp/.tmux.conf ~ && cp -r ~/tmuxtmp/.tmux ~
 rm -rf ~/tmuxtmp
+
