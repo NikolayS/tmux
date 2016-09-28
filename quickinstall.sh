b16d19b14ae50d1fa2434c350cb480458aaf3760
NikolayS <github2@samokhvalov.com>
2016-09-28 06:18:17 +0300

    Update quickinstall.sh

--- quickinstall.sh
+++ quickinstall.sh
@@ -1,6 +1,6 @@
-apt-get -v &> /dev/null && sudo apt update && sudo apt install -y tmux
-which yum &> /dev/null && yum update && yum install tmux
-which brew &> /dev/null && brew install tmux
+apt-get -v &> /dev/null && sudo apt update && sudo apt install -y tmux && sudo apt install mosh
+which yum &> /dev/null && yum update && yum install tmux && sudo yum install mosh
+which brew &> /dev/null && brew install tmux && brew install mosh
 
 git clone https://github.com/NikolayS/tmux.git ~/tmuxtmp
 cp ~/tmuxtmp/.tmux.conf ~ && cp -r ~/tmuxtmp/.tmux ~
